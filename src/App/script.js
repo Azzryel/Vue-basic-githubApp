5d2214258a99af685817dd3e634be27a2b3d1a46
Azzryel <AzryelShroud@gmail.com>
2020-01-12 17:57:14 +0100

    add github application

--- src/App/script.js
+++ src/App/script.js
@@ -1,7 +1,13 @@
+import GithubInput from '../GithubInput/index.vue'
+import GithubOutput from '../GithubOutput/index.vue'
+
 export default {
-    name: 'App',
-    data() {
-      return {}
-    },
-  }
-  
+  name: 'App',
+  components: {
+    'github-input': GithubInput,
+    'github-output': GithubOutput,
+  },
+  data() {
+    return {}
+  },
+}
